957a5d2f4d46078f80637060bac2391ec9c218ca
SmartGirlInUK <minalpadsala846@gmail.com>
2024-04-09 00:43:20 +0100

    push

--- W6-HW/src/Q11.java
+++ W6-HW/src/Q11.java
@@ -1,5 +1,14 @@
 public class Q11 {
+    /*
+    Write a Java program to compute the specified expressions and print the output.
+    Test Data: ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5))
+    Expected Output: 2.138888888888889
+     */
+    //main method
     public static void main(String[] args) {
+        double TestData = ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));//Calculate and store in variable
 
+        System.out.println("Test Data : ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5))\n"+"Expected Output : "+TestData);
+        //print test data and expected output
     }
 }
